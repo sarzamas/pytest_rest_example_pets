35b9605c89c8d3e8328e40061cdc4f85f340354f
sarzamas <sergey.arzamastsev@saymon.info>
2023-12-02 20:43:50 +0300

    swagger fixture /pet

--- tests/pet/conftest.py
+++ tests/pet/conftest.py
@@ -19,24 +19,23 @@ swagger_missing_info = {  # TODO #1 - добавить в Swagger.json эти п
 
 
 @pytest.fixture(scope='class', name='pet_data')
-def class_test_data(test_data, faker) -> Callable:
+def swagger_data(test_data, faker) -> Callable:
     """
     Фикстура подготовки общих данных для выполнения класса тестов
-    TEARDOWN_IDS_POOl: количество используемых этим классом тестов слотов для параметра `test_ids`
-    Все тестовые сущности созданные в тестах как POST с `id` из `test_ids`
-    будут автоматически очищены из базы при teardown
+    Тестовые сущности, созданные в тестах с использованием слотов из `test_ids` будут автоматически удалены при Teardown
+        teardown_pool: количество слотов, необходимых вызывающему классу тестов для параметра `test_ids`
     :param test_data: базовая фикстура подготовки данных для выполнения всех тестовых классов
     :param faker: фикстура подготовки случайных данных
     """
 
-    def _class_test_data(pool, handler, method, context='max') -> Callable:
-        class_test_data = test_data(pool, handler, method)
+    def _swagger_data(teardown_pool, handler, method, context='max') -> Callable:
+        swagger_data = test_data(teardown_pool, handler, method)
 
-        _url = class_test_data['query_data']['url']
-        class_test_data['query_data']['url'] = change_handler(_url) if '/{' in handler else _url
+        _url = swagger_data['query_data']['url']
+        swagger_data['query_data']['url'] = change_handler(_url) if '/{' in handler else _url
 
-        if 'items' in class_test_data['meta']['parameters'][0].keys():
-            swagger_var_params = class_test_data['meta']['parameters']
+        if 'items' in swagger_data['meta']['parameters'][0].keys():
+            swagger_var_params = swagger_data['meta']['parameters']
         elif handler == '/pet' and method == 'POST':  # TODO #1 убрать после решения
             swagger_var_params = swagger_missing_info['parameters']
         else:
@@ -48,13 +47,13 @@ def class_test_data(test_data, faker) -> Callable:
                 var_params[param['name']] = param['items']
 
             count = len([enum for param in var_params for enum in var_params[param]['enum']])
-            assert pool >= count, (
-                f"Необходимо установить число выделяемых слотов (сейчас это {pool}) "
+            assert teardown_pool >= count, (
+                f"Необходимо установить число выделяемых слотов (сейчас это {teardown_pool}) "
                 f"для идентификаторов создаваемых тестом сущностей не меньшим числа возможных вариантов: {count} "
                 f"значений параметра(ов) {var_params.keys()} в текущей версии SWAGGER"
             )
 
-        class_test_data['variables'] = var_params if var_params else None
+        swagger_data['variables'] = var_params if var_params else None
 
         context_min = {
             'name': faker.fwords(),
@@ -79,10 +78,10 @@ def class_test_data(test_data, faker) -> Callable:
         }
 
         if context == 'min':
-            class_test_data['payload'] = context_min
+            swagger_data['payload'] = context_min
         if context == 'max':
-            class_test_data['payload'] = context_min | context_max
+            swagger_data['payload'] = context_min | context_max
 
-        return class_test_data
+        return swagger_data
 
-    return _class_test_data
+    return _swagger_data

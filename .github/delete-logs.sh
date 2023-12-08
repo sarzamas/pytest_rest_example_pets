1277b4659d53037ba6b84792451cdf858c564d23
sarzamas <sergey.arzamastsev@saymon.info>
2023-12-08 22:20:19 +0300

    delete-logs.sh

--- .github/delete-logs.sh
+++ .github/delete-logs.sh
@@ -10,16 +10,16 @@ WORKFLOW_NAME=$2
 
 # Validate arguments
 if [[ -z "$REPOSITORY" ]]; then
-  echo "Repository is required"
+  echo ":octocat: Repository name is required"
   exit 1
 fi
 
 if [[ -z "$WORKFLOW_NAME" ]]; then
-  echo "Workflow name is required"
+  echo ":octocat: Workflow name is required"
   exit 1
 fi
 
-echo "Getting all completed runs for workflow '$WORKFLOW_NAME' in $REPOSITORY..."
+echo ":octocat: Getting all completed runs for workflow '$WORKFLOW_NAME' in $REPOSITORY..."
 
 RUNS=$(
   gh api \
@@ -30,12 +30,12 @@ RUNS=$(
     --jq '.workflow_runs[] | select(.conclusion != "") | .id'
 )
 
-echo "Found $(echo "$RUNS" | wc -l) completed runs for workflow '$WORKFLOW_NAME' - ✅OK!"
+echo ":octocat: Found $(echo "$RUNS" | wc -l) completed runs for workflow '$WORKFLOW_NAME' - ✅OK!"
 echo "$RUNS"
 
 # Delete logs for each run
 for RUN in $RUNS; do
-  echo "Deleting logs for run $RUN..."
+  echo ":octocat: Deleting logs for run $RUN..."
   gh api \
     --silent \
     --method DELETE \
@@ -44,6 +44,13 @@ for RUN in $RUNS; do
     "/repos/$REPOSITORY/actions/runs/$RUN/logs" \
   && \
   echo " - Successfully deleted all logs for run $RUN - ✅OK!" || echo " - Failed to delete logs for run $RUN - ❌NOK!"
+
+
+gh api \
+  --method GET \
+  -H "Accept: application/vnd.github+json" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  "/octocat"
 
   # Sleep for 100ms to avoid rate limiting
   sleep 0.1

2a56d32dca4d81f4cd13d8c0650e3a1e1e7657b8
sarzamas <sarzamas@users.noreply.github.com>
2024-05-16 02:20:51 +0300

    Update hooks.py

--- tests/hooks.py
+++ tests/hooks.py
@@ -6,6 +6,8 @@ from os import path
 import pytest
 from _socket import gethostname
 
+from Utils.RandomData import RandomData as Faker
+from Config import LOG_PATH
 
 @pytest.hookimpl(trylast=True)
 def pytest_configure(config):
@@ -18,7 +20,7 @@ def pytest_configure(config):
     logging_plugin = config.pluginmanager.get_plugin("logging-plugin")
 
     logfile_name = f"{gethostname()}--{Faker.timestamp()}.log"
-    logfile_path = path.join(LOG_DIR, logfile_name)
+    logfile_path = path.join(LOG_PATH, logfile_name)
 
     logging_plugin.set_log_path(logfile_path)
     logging_plugin.log_cli_handler.formatter.add_color_level(logging.INFO, 'cyan')

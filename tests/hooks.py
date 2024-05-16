d4504aca00f9028a424872a07cb2dff00b6cf286
sarzamas <sarzamas@users.noreply.github.com>
2024-05-16 20:14:11 +0300

    Update hooks.py

--- tests/hooks.py
+++ tests/hooks.py
@@ -1,7 +1,7 @@
-""" Хуки вынесены из базового conftest.py в отдельный файл для разделения функций по уровням прикладных задач """
+"""Хуки вынесены из базового conftest в отдельный файл в одной папке для разделения функций по уровням решаемых задач"""
 
 import logging
-from os import linesep, path
+from os import getenv, linesep, path
 
 import pytest
 from _socket import gethostname
@@ -12,27 +12,31 @@ from Utils.RandomData import RandomData as Faker
 
 
 @pytest.hookimpl(trylast=True)
-def pytest_configure(config):
+def pytest_configure(config: pytest.Config):
     """
     Хук для конфигурации тестового прогона:
      - задает имя и путь для локального лог-файла
+     - распределяет лог-файлы между workers при запуске прогона с xdist
      - определяет цвет отображения меток `log_level`: [INFO] в консоли
     :param config: служебная фикстура pytest
     """
     logging_plugin = config.pluginmanager.get_plugin("logging-plugin")
 
-    logfile_name = f"{gethostname()}--{Faker.timestamp()}.log"
-    logfile_path = path.join(LOG_PATH, logfile_name)
-
+    worker_id = getenv('PYTEST_XDIST_WORKER')
+    logfile_name = f"{gethostname()}--{Faker.timestamp()}"
+    logfile_name = f"{logfile_name}__{worker_id}" if worker_id else f"{logfile_name}"
+    logfile_path = path.join(LOG_PATH, f"{logfile_name}.log")
     logging_plugin.set_log_path(logfile_path)
+
     logging_plugin.log_cli_handler.formatter.add_color_level(logging.INFO, 'cyan')
 
 
 @pytest.fixture(autouse=True)
-def log_delimiter(request):
+def log_delimiter(request: pytest.Subrequest):
     """
     Разделитель строк текста между тестами в лог-файле тестового прогона
+    :param request: служебная фикстура pytest
     """
     test_name = request.function.__name__
     test_path = request.fspath.strpath
-    logger.info(f"{linesep}{test_name:-^79} from {test_path}")
+    logger.info(f"{linesep}{(' ' + test_name + ' '):-^79} {test_path}")

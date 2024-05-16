767085e1523a0845e4b781614966031b5d91cd15
sarzamas <sarzamas@users.noreply.github.com>
2024-05-16 05:14:39 +0300

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,4 @@
-__all__ = ['pytest_configure']
+__all__ = ['log_delimiter', 'pytest_configure']
 
 from datetime import datetime
 from os import linesep
@@ -14,7 +14,7 @@ from Helpers.RequestsHelper import TestTimeout
 from tests import change_handler
 from Utils.RandomData import RandomData
 
-from .hooks import pytest_configure
+from .hooks import log_delimiter, pytest_configure
 
 
 @pytest.fixture(scope='session', name='test_data')

37ca50c6677b0819e4c861cbf86efad924f0eb89
sarzamas <sarzamas@users.noreply.github.com>
2024-05-20 18:14:14 +0300

    Update functions.py

--- Utils/functions.py
+++ Utils/functions.py
@@ -21,7 +21,7 @@ def clear_empty_in_folder(folder: str):
             clear_empty_in_folder(entity_path)
 
 
-def make_text_bold(text: str, is_tty: bool = stdin.isatty()) -> str:
+def make_text_ansi_bold(text: str, is_tty: bool = stdin.isatty()) -> str:
     """
     Функция для выделения текста жирным с помощью меток ANSI-color:
      - для дифференциации форматирования теста в зависимости от места назначения вывода (в окно IDE или в логфайл)
@@ -30,20 +30,38 @@ def make_text_bold(text: str, is_tty: bool = stdin.isatty()) -> str:
     :param is_tty: stdin.isatty() - признак процесса, инициировавшего запуск тестовой сессии:
      - True - инициатор запуска - консоль терминала (как в CI) -> подразумевается вывод в логфайл - без меток ANSI color
      - False - запуск производился из окна IDE -> подразумевается вывод в окно IDE - с метками ANSI color
-    :return str: bold text
+    :return: str: ANSI bold text
     """
     return '\033[1m%s\033[0m' % text if not is_tty else text
+
+
+def make_text_ansi_plain(text) -> str:
+    """
+    Функция убирает все метки ANSI color из текста для логирования его в файл в виде plain/text
+     - сигнатуры меток добавлены по факту их обнаружения в логфайле
+    :param text: исходный текст
+    :return: str: текст, очищенный от меток ANSI color
+    """
+    return (
+        text
+        .replace('\033[34;1m', '')
+        .replace('\033[32;1m', '')
+        .replace('\033[31;1m', '')
+        .replace('\033[1;1m', '')
+        .replace('\033[0m', '')
+    )
 
 
 def make_text_wrapped(text: str, wrap_symbol: str = '-', width: int = 79, space: int = 1, new_line: bool = True) -> str:
     """
     Функция для форматирования текста в строку с дополнением одинаковыми символами до нужной ширины (Centered)
+     - Example: `---------- example ----------`
     :param text: исходный текст
     :param width: итоговая ширина форматированного текста с обёрткой его символами
     :param wrap_symbol: символ для обертки текста
     :param space: количество пробелов между текстом и `wrap_symbol`
     :param new_line: ключ для выдачи результата с новой строки
-    :return str: bold text
+    :return: str: wrapped text
     """
     ls = linesep if new_line else None
     text = ' ' * space + text + ' ' * space

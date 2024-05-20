77cf87b13178f10cd934a1eabd96d053fa818d11
sarzamas <sarzamas@users.noreply.github.com>
2024-05-20 09:07:07 +0300

    Update logger_hook.py

--- tests/logger_hook.py
+++ tests/logger_hook.py
@@ -8,7 +8,11 @@ import pytest
 from _socket import gethostname
 
 from Config import DEBUG, LOG_PATH
-from Utils.functions import clear_empty_in_folder, make_text_bold, make_text_wrapped
+from Utils.functions import (
+                             clear_empty_in_folder,
+                             make_text_bold,
+                             make_text_wrapped
+)
 from Utils.RandomData import RandomData as Faker
 
 

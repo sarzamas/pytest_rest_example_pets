2784ed411c3d69a4ac77a2323a5d4ad2e39b7d61
sarzamas <sarzamas@users.noreply.github.com>
2024-05-20 12:48:49 +0300

    Update functions.py

--- Utils/functions.py
+++ Utils/functions.py
@@ -4,17 +4,18 @@ from os import linesep, listdir, path, remove
 def clear_empty_in_folder(folder: str):
     """
     Рекурсивное удаление всех пустых сущностей в текущей директории
+     - игнорирование ошибки: в случае, если удаление невозможно - продолжаем двигаться дальше без удаления
     :param folder: папка от которой включительно и вниз начинается поиск
     """
     dir_list = listdir(folder)
     for entity in dir_list:
         entity_path = path.join(folder, entity)
         if path.isfile(entity_path):
-            try:
-                if not path.getsize(entity_path):
+            if not path.getsize(entity_path):
+                try:
                     remove(entity_path)
-            except PermissionError:
-                continue
+                except PermissionError:
+                    continue
         elif path.isdir(entity_path):
             clear_empty_in_folder(entity_path)
 

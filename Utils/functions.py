a99518c1b9dff6b09d4337665b072df677e04155
sarzamas <sarzamas@users.noreply.github.com>
2024-05-20 08:20:05 +0300

    Update functions.py

--- Utils/functions.py
+++ Utils/functions.py
@@ -17,3 +17,30 @@ def clear_empty_in_folder(folder: str):
                 continue
         elif path.isdir(entity_path):
             clear_empty_in_folder(entity_path)
+
+
+def make_text_bold(text: str) -> str:
+    """
+    Функция для выделения текста жирным с помощью меток ANSI-color
+     - для форматирования строк лога
+     - может сочетаться с другими метками ANSI-color
+    :param text: исходный текст
+    :return str: bold text
+    """
+    return '\033[1m%s\033[0m' % text
+
+
+def make_text_wrapped(text: str, wrap_symbol: str = '-', width: int = 79, space: int = 1, new_line: bool = True) -> str:
+    """
+    Функция для форматирования текста в строку с дополнением одинаковыми символами до нужной ширины (Centered)
+    :param text: исходный текст
+    :param width: итоговая ширина форматированного текста с обёрткой его символами
+    :param wrap_symbol: символ для обертки текста
+    :param space: количество пробелов между текстом и `wrap_symbol`
+    :param new_line: ключ для выдачи результата с новой строки
+    :return str: bold text
+    """
+    ls = linesep if new_line else None
+    text = ' ' * space + text + ' ' * space
+    return f"{ls or ''}{text:{wrap_symbol}^{width}}"
+    

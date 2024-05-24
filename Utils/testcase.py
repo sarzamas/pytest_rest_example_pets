4c022ef61e5b69acbadc421f68ca4bba84ed0ddb
sarzamas <sarzamas@users.noreply.github.com>
2024-05-24 20:39:18 +0300

    Update testcase.py

--- Utils/testcase.py
+++ Utils/testcase.py
@@ -27,33 +27,33 @@ def allure_testcase(title: str, url: str = None, name: str = "Ссылка на 
     return wrapper
 
 
-def allure_story(title: str, url: str = None, name: str = "Ссылка на сторю в Jira", parametrized_func=True):
+def allure_story(title: str, url: str = None, name: str = "Ссылка на сторю в Jira", parametrized_func: bool = True):
     """
-    Декоратор. Заменяет собой @allure.story, @allure.link
+    Декоратор. Заменяет собой `@allure.story`, `@allure.link`
     ```
     @allure_testcase("Название", "https://ссылка.на.сторю/PROJ-1234")
-    равносильно
+    эквивалентно:
     @allure.story("PROJ-1234 Название")
     @allure.link("https://ссылка.на.сторю/PROJ-1234", name="Ссылка на сторю в Jira")
     ```
-    Применяется, когда необходимо объединить несколько тестов (в т.ч. содержащихся в классе) в одну сторю внутри
-    представления Behaviors.
-    Если применяется к функции - необходимо выставить флаг parametrized_case, тогда каждый размноженный тест с
-    параметром получит имя стори (т.к. по сути это один и тот же тест) и отпадет необходимость ставить декоратор
-    @allure_testcase
+    Применяется для объединения в Allure отчете, в представлении `Behaviors`, нескольких тестов под одним заголовком:
+       - для одного параметризованного теста - `parametrized_func = True`
+     или
+       - для нескольких не параметризованных тестов (в т.ч. содержащихся в разных классах) - `parametrized_func = False`
+    - объединять таким декоратором параметризованные и не параметризованные тестовые функции нельзя!
+    - при этом отсутствует необходимость в декораторе `@allure_testcase`
     ---
     :param title: (str): Имя, отражаемое в Allure-отчете. Аналогично @allure.story
     :param url: (str, optional): Ссылка, отражаемая в Allure-отчете. Аналогично @allure.link
     :param name: (str, optional): Имя ссылки, отражаемое в Allure-отчете. Аналогично @allure.link
-    :param parametrized_func: (bool, optional): Флаг, который необходимо ставить, если декоратор вешается на одну
-                                               параметризованную функцию
+    :param parametrized_func: (bool, optional): Признак применения для одной параметризованной тестовой функции
     """
 
     def wrapper(function):
         new_title = title
         if url:
             function = allure.link(url, name=name)(function)
-            if (story_num := url.split('/')[-1]).startswith("SED-"):
+            if (story_num := url.split('/')[-1]).startswith("PROJ-"):
                 new_title = f"{story_num} {title}"
         function = allure.story(new_title)(function)
         return allure.title(new_title)(function) if parametrized_func else function

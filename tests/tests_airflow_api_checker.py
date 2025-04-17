e03884a44d7ab8c8944587b3cb0f5bb0d86e0249
sarzamas <sarzamas@users.noreply.github.com>
2025-04-17 16:15:55 +0300

    Update tests_airflow_api_checker.py

--- tests/tests_airflow_api_checker.py
+++ tests/tests_airflow_api_checker.py
@@ -8,7 +8,7 @@ from libs.api.airflow.checker import Checker
 @pytest.mark.airflow
 class TestDAGRun:
 
-    def test_get_dags_list(self, airflow_client):
+    def test_get_dags_list(self, api_client):
         """
         Получение списка DAGs: GET /dags
         Ex:
@@ -19,9 +19,9 @@ class TestDAGRun:
         # Arrange
         url = "dags"
         # Act
-        resp = airflow_client.get(url=url)
+        resp = api_client.get(url=url)
         # Check
-        Checker.validate_response_json(
+        dags = Checker.validate_response_json(
             resp,
             required_keys=[
                 "dags.[*].dag_id",
@@ -48,6 +48,10 @@ class TestDAGRun:
                 # "dags.[*].owners.[:]": str,  # неверный синтаксис - OK!
             },
         )
+        assert "dags" in dags, "Ключ 'dags' отсутствует в ответе"
+        assert isinstance(dags["dags"], list), "DAGs должны быть списком"
+        assert "total_entries" in dags, "Нет информации о количестве DAG"
+
         print(Checker.get_value(resp.json(), key_path="total_entries"))
         print(Checker.get_value(resp.json(), key_path="dags.[0].is_paused", unique=True))
         print(Checker.get_value(resp.json(), key_path="dags.[*].is_paused", unique=True))
@@ -57,7 +61,7 @@ class TestDAGRun:
         print(Checker.get_value(resp.json(), key_path="dags.[*].owners.[*]", unique=True))
         print(Checker.get_value(resp.json(), key_path="dags.[*].tags.[*].name", unique=True))
 
-    def test_get_dag_by_id(self, airflow_client):
+    def test_get_dag_by_id(self, api_client, test_dag_id):
         """
         Получение данных о DAG по ID: GET /dags/{dag_id}
         Ex:
@@ -66,11 +70,11 @@ class TestDAGRun:
             -H 'Content-Type: application/json'
         """
         # Arrange
-        url = "/////dags/////person_hdfs_s3"  # проверка на нормализацию `/` в url - OK!
+        url = f'/////dags/////{test_dag_id}'  # проверка на нормализацию `/` в url - OK!
         # Act
-        resp = airflow_client.get(url=url)
+        resp = api_client.get(url=url)
         # Check
-        Checker.validate_response_json(
+        dag = Checker.validate_response_json(
             resp,
             required_keys=[
                 "dag_id",
@@ -93,6 +97,9 @@ class TestDAGRun:
 
             },
         )
+        assert "dag_id" in dag, "Ключ 'dag_id' отсутствует в ответе"
+        assert dag["dag_id"] == test_dag_id, "Значение ключа 'dag_id' не соответствует ожиданию"
+
         print(Checker.get_value(resp.json(), key_path="dag_id"))
         print(Checker.get_value(resp.json(), key_path="is_paused"))
         print(Checker.get_value(resp.json(), key_path="tags.[*].name", unique=True))
@@ -100,25 +107,27 @@ class TestDAGRun:
         print(Checker.get_value(resp.json(), key_path="total_entries"))  # ключа нет - None!
         print(Checker.get_value(resp.json(), key_path="dags.[0].is_paused", unique=True))  # ключа нет - None!
 
-    def test_patch_dag_paused(self, airflow_client):
+    @pytest.mark.parametrize("is_paused, expected_status",
+                             [(True, "ОСТАНОВЛЕН"), (False, "ЗАПУЩЕН")], ids=["ОСТАНОВКА DAG", "ЗАПУСК DAG"])
+    def test_dag_control(self, api_client, test_dag_id, is_paused, expected_status):
         """
-        Остановка DAG: PATCH /dags/{dag_id}
+        Остановка/Запуск DAG: PATCH /dags/{dag_id}
         Ex:
             curl -k --user 'api-user:api-user' -X PATCH \
             https://airflow-forge.apps.qa.kryptodev.ru/api/v1/dags/person_hdfs_s3?update_mask=is_paused \
             -H 'Content-Type: application/json' \
             -d '{
-                "is_paused": true
+                "is_paused": true/false
             }'
         """
         # Arrange
-        url = "dags/person_hdfs_s3"
+        url = f'dags/{test_dag_id}'
         params = {"update_mask": "is_paused"}
-        payload = {"is_paused": True}
+        payload = {"is_paused": is_paused}
         # Act
-        resp = airflow_client.patch(url=url, params=params, data=payload)
+        resp = api_client.patch_dag(url=url, params=params, data=payload)
         # Check
-        Checker.validate_response_json(
+        dag = Checker.validate_response_json(
             resp,
             required_keys=[
                 "dag_id",
@@ -129,40 +138,13 @@ class TestDAGRun:
                 "is_paused": bool,
             },
         )
-        print(Checker.get_value(resp.json(), key_path="dag_id"))
-        print(Checker.get_value(resp.json(), key_path="is_paused"))
-        print(Checker.get_value(resp.json(), key_path="dags.[0].is_paused", unique=True))
+        assert "dag_id" in dag, "Ключ 'dag_id' отсутствует в ответе"
+        assert dag["dag_id"] == test_dag_id, "Значение ключа 'dag_id' не соответствует ожиданию"
+        assert "is_paused" in dag, "Ответ не содержит is_paused"
+        assert dag["is_paused"] is is_paused, f'Ожидалось что DAG должен быть {expected_status}'
 
-    def test_patch_dag_un_paused(self, airflow_client):
-        """
-        Запуск DAG: PATCH /dags/{dag_id}
-        Ex:
-            curl -k --user 'api-user:api-user' -X PATCH \
-            https://airflow-forge.apps.qa.kryptodev.ru/api/v1/dags/person_hdfs_s3?update_mask=is_paused \
-            -H 'Content-Type: application/json' \
-            -d '{
-                "is_paused": false
-            }'
-        """
-        # Arrange
-        url = "dags/person_hdfs_s3"
-        params = {"update_mask": "is_paused"}
-        payload = {"is_paused": False}
-        # Act
-        resp = airflow_client.patch(url=url, params=params, data=payload)
-        # Check
-        Checker.validate_response_json(
-            resp,
-            required_keys=[
-                "dag_id",
-                "is_paused",
-            ],
-            key_types={
-                "dag_id": str,
-                "is_paused": bool,
-            },
-        )
         print(Checker.get_value(resp.json(), key_path="dag_id"))
         print(Checker.get_value(resp.json(), key_path="is_paused"))
         print(Checker.get_value(resp.json(), key_path="is_paused.[*]"))
+        print(Checker.get_value(resp.json(), key_path="dags.[0].is_paused", unique=True))
         print(Checker.get_value(resp.json(), key_path="is_paused.[*]", unique=True))
